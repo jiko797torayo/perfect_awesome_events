9e9f63288f2b7b2663dd33c775c1bf36b05eac59
jiko797torayo <oec7dwayne7kj@gmail.com>
2020-08-03 21:56:18 +0900

    add/画像の追加・編集・表示

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,11 @@
 class Event < ApplicationRecord
   belongs_to :owner, class_name: "User"
   has_many :tickets, dependent: :destroy
+  has_one_attached :image
+
+  attr_accessor :remove_image
+
+  before_save :remove_image_if_user_accept
 
   validates :name, length: { maximum: 50 }, presence: true
   validates :place, length: { maximum: 100 }, presence: true
@@ -15,6 +20,10 @@ class Event < ApplicationRecord
   end
 
   private
+
+  def remove_image_if_user_accept
+    self.image = nil if ActiveRecord::Type::Boolean.new.cast(remove_image)
+  end
 
   def start_at_should_be_before_end_at
     return unless start_at && end_at
