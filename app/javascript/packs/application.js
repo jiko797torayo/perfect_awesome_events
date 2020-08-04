28f9c8847754e4ef87b83bc650f26bce8cd56679
jiko797torayo <oec7dwayne7kj@gmail.com>
2020-08-05 02:10:12 +0900

    add/ElasticSearchによる検索機能を追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,6 @@
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
+require("get_form_turbolinks")
 import "bootstrap"
 import "bootstrap/scss/bootstrap.scss"
